99ea36ef4db84bc766b3b36834f8cc1efd3dde92
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2018-01-03 22:20:52 +0800

    Menampilkan Bobot W

--- SAW3.cpp
+++ SAW3.cpp
@@ -45,4 +45,10 @@ int main(){
 			cout<<Ra[i][j]<<" | ";
 		}cout<<endl;
 	}
+	
+	cout<<endl<<"BOBOT W : "<<endl;
+	double w[5]={5,3,4,4,2};
+	for(int i=0;i<5;i++){
+	cout<<w[i]<<" | ";
+	}cout<<endl;
 }
