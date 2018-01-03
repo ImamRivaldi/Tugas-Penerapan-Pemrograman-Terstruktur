671abb5ae5c9c2ef2ec0eb70e75f8289c6b20304
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2018-01-03 19:38:43 +0800

    Deklarasi Variable

--- SAW1.cpp
+++ SAW1.cpp
@@ -4,5 +4,21 @@
 using namespace std;
 
 int main(){
+	double a1[5]={4,4,5,3,3};
+	double a2[5]={3,3,4,2,3}; 
+	double a3[5]={5,4,2,2,2};
 	
+	double Ra1[5];
+	double Ra2[5];
+	double Ra3[5];
+	
+	double RWa1[5];
+	double RWa2[5];
+	double RWa3[5];
+		
+	double w[5]={5,3,4,4,2};
+	double m[5];
+	double ha[3];
+	
+	int i;
 }
